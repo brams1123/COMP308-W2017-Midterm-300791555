d2dc5b12f02278694ba12117535500346605ffb1
hosifo <300791555@cencol.ca>
2017-02-25 15:44:54 -0500

    updating ...

--- server/routes/books.js
+++ server/routes/books.js
@@ -28,8 +28,7 @@ router.get('/', (req, res, next) => {
 router.get('/add', (req, res, next) => {
     res.render('books/details', {
     title: "Add a new Book",
-    books: '',
-    displayName: req.user ? req.user.displayName : ''
+    books: ''
   });
 
 });
